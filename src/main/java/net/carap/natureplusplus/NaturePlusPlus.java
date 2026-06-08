ff2830dbf9ab119e88cf6fca921cdfd9fb057b2a
CarapanceanuAlex <147550122+CarapanceanuAlex@users.noreply.github.com>
2026-06-09 00:36:39 +0300

    initial commit

---
+++ src/main/java/net/carap/natureplusplus/NaturePlusPlus.java
@@ -0,0 +1,17 @@
+package net.carap.natureplusplus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NaturePlusPlus implements ModInitializer {
+	public static final String MOD_ID = "natureplusplus";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
